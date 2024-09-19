570da8cadb6accf2fca777fbad7b2a6492db6f9e
shadpre <39797034+shadpre@users.noreply.github.com>
2024-09-19 13:38:39 +0200

    Refactor project structure and add Tailwind CSS configuration

---
+++ Client/src/modules/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() : JSX.Element {
+return(
+<footer className="fixed bottom-0 left-0 w-full bg-white shadow-md z-50">
+        <div className="container mx-auto p-4">
+          <p>Footer</p>
+        </div>
+      </footer>
+)}
---
+++ Client/src/modules/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() : JSX.Element {
+    return(
+        <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+        <div className="container mx-auto p-4">
+          <h1>Header</h1>
+        </div>
+      </header>
+    )}
